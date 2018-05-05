f58c366b14c2ff377719949841387e853dc315c7
ZhuDengquan <dengquanzhu@gmail.com>
2018-05-05 09:52:33 +0800

    更新一些文件

--- BlankRealm/ViewController.swift
+++ BlankRealm/ViewController.swift
@@ -10,28 +10,12 @@ import UIKit
 import RealmSwift
 
 class ViewController: UIViewController {
-    
-    func test() {
-        // setup
-        let realm = try! Realm(configuration:
-            Realm.Configuration(inMemoryIdentifier: "TemporaryRealm"))
-        try! TestDataSet.create(in: realm)
-        
-        print("Ready to play!")
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        test()
+        view.backgroundColor = UIColor.cyan
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
